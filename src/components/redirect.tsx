177d138bf3cbfa7869f4207cb5a3aff39fa41c3b
Daniel Volchek <danielvolchek123@gmail.com>
2023-02-13 14:08:15 -0500

    changed imaredirect to redirect and built carousel component: TODO work on animation

---
+++ src/components/redirect.tsx
@@ -0,0 +1,129 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Redirections = [
+  { name: "Full Stack Dev", path: "/portfolio" },
+  { name: "Skateboarder", path: "/skater" },
+  { name: "Rock Climber", path: "/rockclimber" },
+  { name: "Ramen Lover", path: "/ramen" },
+];
+
+// time in ms to rotate carousel
+const MovementTime = 1500;
+
+type Props = {};
+
+const VerticalCarousel = ({}: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(MovementTime);
+
+  const setIndexFromDot = (index: number) => {
+    // add delay of 1 second after pressing a button
+    const DELAY = 1000;
+    setCurrentIndex(index);
+    setRemainingTime(MovementTime + DELAY);
+  };
+
+  useEffect(() => {
+    const TICK = 10;
+
+    const timeout = setTimeout(() => {
+      setRemainingTime((remainingTime) => remainingTime - TICK);
+      if (remainingTime <= 0) {
+        setCurrentIndex(
+          (currentIndex) => (currentIndex + 1) % Redirections.length
+        );
+        setRemainingTime(MovementTime);
+      }
+    }, TICK);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [remainingTime]);
+
+  return (
+    <div className="flex items-center">
+      <div className="flex flex-col">
+        <h3 className="text-6xl">I'm a...</h3>
+        <Link
+          className={`text-8xl ${
+            currentIndex % 2 === 0 ? "text-thulianpink" : "text-hunyadiyellow"
+          } hover:underline mr-8 
+          min-w-[35vw]
+          transition-colors duration-700 ease-in-out
+          `}
+          href={Redirections[currentIndex].path}
+        >
+          {Redirections[currentIndex].name}
+        </Link>
+      </div>
+      <DotContainer
+        currentIndex={currentIndex}
+        setCurrentIndex={setIndexFromDot}
+      />
+    </div>
+  );
+};
+
+type DotContainerProps = {
+  currentIndex: number;
+  setCurrentIndex: (index: number) => void;
+};
+const DotContainer = ({ currentIndex, setCurrentIndex }: DotContainerProps) => {
+  return (
+    <div className="-ml-16">
+      {Redirections.map((_, index) => {
+        return (
+          <Dot
+            currentIndex={currentIndex}
+            dotIndex={index}
+            key={index}
+            setCurrentIndex={setCurrentIndex}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const Dot = ({
+  currentIndex,
+  dotIndex,
+  setCurrentIndex,
+}: {
+  currentIndex: number;
+  dotIndex: number;
+  setCurrentIndex: (index: number) => void;
+}) => {
+  const pressedDot = () => {
+    setCurrentIndex(dotIndex);
+  };
+
+  return (
+    <div
+      className={`rounded-full w-8 h-8 ${
+        currentIndex === dotIndex ? "bg-lapizlazuli" : "bg-none"
+      } border-solid border-4 border-black my-4 cursor-pointer`}
+      onClick={pressedDot}
+    ></div>
+  );
+};
+
+export default VerticalCarousel;
+
+// import React, { useState, useEffect } from "react";
+//
+// const Redirections = [
+//   { name: "Full Stack Dev", path: "/portfolio" },
+//   { name: "Skater", path: "/skater" },
+//   { name: "Rock Climber", path: "/rockclimber" },
+//   { name: "Ramen Lover", path: "/ramen" },
+// ];
+//
+// const Carousel = () => {
+//   const [index, setIndex] = useState(0);
+//
+// };
+//
+// export default Carousel;
