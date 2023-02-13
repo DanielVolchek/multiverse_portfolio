4fe46f317b609f5a6b54c5e059f5012c53d7708d
Daniel Volchek <danielvolchek123@gmail.com>
2023-02-13 11:15:43 -0500

    added custom color palette generated from coolors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // generated on coolors.co using tailwind bg-teal-200 (iceblue) as the primary color
+        // https://coolors.co/33658a-2f4858-99f6e4-f6ae2d-f26419
+        lapizlazuli: "#33658a",
+        charcoal: "#2f4858",
+        iceblue: "#99f6e4",
+        hunyadiyellow: "#f6ae2d",
+        thulianpink: "#ce7da5",
+      },
+    },
   },
   plugins: [],
 };
