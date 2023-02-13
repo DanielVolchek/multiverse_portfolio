f6e8a8b498df3b2b4e758afdd46c3ff5ab7aa5c8
Daniel Volchek <danielvolchek123@gmail.com>
2023-02-13 11:15:15 -0500

    started redirect file

---
+++ src/components/imaredirect.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Redirections = [
+  { name: "Full Stack Dev", path: "/portfolio" },
+  { name: "Skater", path: "/skater" },
+  { name: "Rock Climber", path: "/rockclimber" },
+  { name: "Ramen Lover", path: "/ramen" },
+];
+
+const redirect = ({}: Props) => {};
+
+export default redirect;
